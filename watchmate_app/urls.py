e45eb44be354c4de9aaace7dd7ec9ff86e349476
Abdul Aziz <abdulaziz.eu1@gmail.com>
2025-07-26 23:27:58 +0600

    project init and some get API to test how menually create API

---
+++ watchmate_app/urls.py
@@ -0,0 +1,7 @@
+from  django.urls import path
+from watchmate_app.views import movie_list, movie_by_id
+
+urlpatterns = [
+    path('list/', movie_list, name="movie-list"),
+    path('<int:pk>', movie_by_id, name="movie-details" )
+]
---
+++ watchmate_app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import Movie
+from django.http import JsonResponse
+
+def movie_list(request):
+    movies = Movie.objects.all()
+    data = {"movies" :  list(movies.values())}
+    return JsonResponse(data)
+
+def movie_by_id(request, pk):
+    movie = Movie.objects.get(pk = pk)
+    date = {
+        "name" : movie.name,
+        "description": movie.description,
+        "isActive" : movie.isActive
+        }
+    return JsonResponse(date)
