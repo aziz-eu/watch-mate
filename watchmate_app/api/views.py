bb999cfb2b14e45dbf398f01bee45892bd41e68a
Abdul Aziz <abdulaziz.eu1@gmail.com>
2025-07-27 22:10:31 +0600

    create api to get movie by id

--- watchmate_app/api/views.py
+++ watchmate_app/api/views.py
@@ -9,6 +9,8 @@ def movie_list(request):
     movies = Movie.objects.all()
     serializer = MovieSerializers(movies, many=True)
     return Response(serializer.data)
-
-def movie_by_id(resqust):
-    pass
+@api_view()
+def movie_by_id(resqust, pk):
+    movie = Movie.objects.get(pk = pk)
+    serializer = MovieSerializers(movie)
+    return Response(serializer.data)
