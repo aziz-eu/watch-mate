b9b9a5a76f71cd749206b1836e75904a9512719b
Abdul Aziz <abdulaziz.eu1@gmail.com>
2025-07-30 23:50:36 +0600

    add review model

--- watchmate_app/models.py
+++ watchmate_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
@@ -11,6 +12,16 @@ class StreamPlatform(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Review(models.Model):
+    reating = models.PositiveBigIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    reviewText = models.CharField(max_length=200)
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
 
 class WatchList(models.Model):
